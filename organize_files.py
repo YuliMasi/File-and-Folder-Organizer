e1598185a227132c9843bc499e25f5258e3327f3
YuliMasi <yulianamasisoporta@gmail.com>
2026-08-02 21:23:02 -0600

    imports and folder paths

---
+++ organize_files.py
@@ -0,0 +1,6 @@
+import os # the code communicates with the operating system (create, delete or move files)
+import shutil #it's for copy(), copytree(), move(), rmtree(), make_archive() the  diferents files
+from pathlib import Path #It allows you to manage file system paths
+
+#file path to sort example
+file_path = r"C:\Users\Yuliana\Downloads\Pictures"
